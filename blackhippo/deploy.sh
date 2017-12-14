43a06d30aea855239f275bd54ccc83bec1aadcf7
Martin Urbanec <martin@urbanec.cz>
2017-12-14 13:49:44 +0000

    Publish when deploy

--- blackhippo/deploy.sh
+++ blackhippo/deploy.sh
@@ -3,4 +3,5 @@
 sudo -u www-data kill $(cat /tmp/blackhippo.pid)
 sudo -u www-data rm /tmp/blackhippo.pid
 sudo -u www-data git pull
+sudo -u www-data dotnet publish
 sudo -u www-data start-stop-daemon --start --background -m --pidfile /tmp/blackhippo.pid --exec /usr/libexec/blackhippo --
